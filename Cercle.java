ead252f64ab3d2abcecd89a8bff474f9e42d4e16
deniseveca <dveca1@gmail.com>
2019-05-09 15:39:20 +0200

    cercle funcions

--- Cercle.java
+++ Cercle.java
@@ -17,7 +17,18 @@ public class Cercle {
         this.x = c.x;
         this.y = c.y;
     }
-	 
+	public float getX(){
+        return this.x;
+    }  
+    public float getY(){
+        return this.y;
+    }  
+    public void setX(float a){
+        this.x=a;
+    }        
+    public void setY(float b){
+        this.y=b;
+    } 
 	public String toString(){
         return ("X és " +this.x+ " i Y és "+ this.y);
     }
@@ -29,7 +40,7 @@ public class Cercle {
 	
 	    public static void main(String[] args) {
 
+
     }
 
 }
-
